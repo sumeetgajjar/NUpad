718bc6a207007203b883513cd5b4a6172b218487
Sumeet Gajjar <sumeetgajjar93@gmail.com>
2020-11-21 21:39:21 -0800

    1. update GTest version 2. add VectorClock implementation and tests

--- crdt_lib/src/clock.cpp
+++ crdt_lib/src/clock.cpp
@@ -8,11 +8,40 @@
 
 namespace nupad::clock {
 
-    bool VectorClock::initialized = false;
+    PeerId VectorClock::myPeerId_;
+    bool VectorClock::initialized_ = false;
+    ClockState VectorClock::clockState_(10);
 
-    void VectorClock::init() {
-        CHECK(!initialized) << "VectorClock already initialized";
-        initialized = true;
+    void VectorClock::init(PeerId myPeerId) {
+        CHECK(!VectorClock::initialized_) << "VectorClock already initialized";
+        myPeerId_ = std::move(myPeerId);
+        CHECK_STRNE(myPeerId_.c_str(), "") << "myPeerId cannot be empty";
+        clockState_[myPeerId_] = 0;
+        initialized_ = true;
         LOG(INFO) << "VectorClock initialized";
+    }
+
+    void VectorClock::tick(const Tick tick) {
+        CHECK(initialized_) << "VectorClock not initialized";
+        clockState_.at(myPeerId_) += tick;
+    }
+
+    void VectorClock::update(const PeerId &peerId, const Tick tick) {
+        CHECK(initialized_) << "VectorClock not initialized";
+        if (clockState_.find(peerId) != clockState_.end()) {
+            CHECK_LT(clockState_.at(peerId), tick)
+                << "new tick value should be strictly greater than current tick value";
+        }
+        clockState_[peerId] = tick;
+    }
+
+    Tick VectorClock::getTick(const PeerId &peerId) {
+        CHECK(initialized_) << "VectorClock not initialized";
+        return clockState_.at(peerId);
+    }
+
+    ClockState VectorClock::getState() {
+        CHECK(initialized_) << "VectorClock not initialized";
+        return ClockState(clockState_);
     }
 }
